f1a44b87b23b47418f7a2534d5a386ecd03546ee
monlie <woaixiaolei1314>
2018-07-27 17:28:06 +0800

    add

--- broker/src/main/java/org/apache/rocketmq/broker/latency/BrokerFixedThreadPoolExecutor.java
+++ broker/src/main/java/org/apache/rocketmq/broker/latency/BrokerFixedThreadPoolExecutor.java
@@ -17,36 +17,32 @@
 
 package org.apache.rocketmq.broker.latency;
 
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.RejectedExecutionHandler;
-import java.util.concurrent.RunnableFuture;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class BrokerFixedThreadPoolExecutor extends ThreadPoolExecutor {
+
     public BrokerFixedThreadPoolExecutor(final int corePoolSize, final int maximumPoolSize, final long keepAliveTime,
-        final TimeUnit unit,
-        final BlockingQueue<Runnable> workQueue) {
+                                         final TimeUnit unit,
+                                         final BlockingQueue<Runnable> workQueue) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
     }
 
     public BrokerFixedThreadPoolExecutor(final int corePoolSize, final int maximumPoolSize, final long keepAliveTime,
-        final TimeUnit unit,
-        final BlockingQueue<Runnable> workQueue, final ThreadFactory threadFactory) {
+                                         final TimeUnit unit,
+                                         final BlockingQueue<Runnable> workQueue, final ThreadFactory threadFactory) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory);
     }
 
     public BrokerFixedThreadPoolExecutor(final int corePoolSize, final int maximumPoolSize, final long keepAliveTime,
-        final TimeUnit unit,
-        final BlockingQueue<Runnable> workQueue, final RejectedExecutionHandler handler) {
+                                         final TimeUnit unit,
+                                         final BlockingQueue<Runnable> workQueue, final RejectedExecutionHandler handler) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, handler);
     }
 
     public BrokerFixedThreadPoolExecutor(final int corePoolSize, final int maximumPoolSize, final long keepAliveTime,
-        final TimeUnit unit,
-        final BlockingQueue<Runnable> workQueue, final ThreadFactory threadFactory,
-        final RejectedExecutionHandler handler) {
+                                         final TimeUnit unit,
+                                         final BlockingQueue<Runnable> workQueue, final ThreadFactory threadFactory,
+                                         final RejectedExecutionHandler handler) {
         super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue, threadFactory, handler);
     }
 
