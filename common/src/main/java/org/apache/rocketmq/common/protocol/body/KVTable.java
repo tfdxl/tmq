59244ce886e7a9931b9757a5f50286928f130d1c
tianfeng <1114089877@qq.com>
2018-07-27 23:01:45 +0800

    add comment

--- common/src/main/java/org/apache/rocketmq/common/protocol/body/KVTable.java
+++ common/src/main/java/org/apache/rocketmq/common/protocol/body/KVTable.java
@@ -16,10 +16,12 @@
  */
 package org.apache.rocketmq.common.protocol.body;
 
-import java.util.HashMap;
 import org.apache.rocketmq.remoting.protocol.RemotingSerializable;
 
+import java.util.HashMap;
+
 public class KVTable extends RemotingSerializable {
+
     private HashMap<String, String> table = new HashMap<String, String>();
 
     public HashMap<String, String> getTable() {
